7250f5533dacf98528e2b5124eb9f92555a35cc9
Gabriel Schubiner <gabriel.schubiner@childmind.org>
2024-05-23 12:34:21 -0400

    Add imports to package __init__.py

--- src/bidsi/__init__.py
+++ src/bidsi/__init__.py
@@ -1,1 +1,15 @@
-"""Generate BIDS Structure."""
+"""BIDS Structure Generation Library."""
+
+from .bids_conversion import Bidsifiable
+from .bids_model import BidsBuilder, BidsConfig, BidsEntity, BidsModel
+from .bids_writer import BidsWriter, MergeStrategy
+
+__all__ = [
+    "BidsWriter",
+    "MergeStrategy",
+    "BidsBuilder",
+    "BidsConfig",
+    "BidsEntity",
+    "BidsModel",
+    "Bidsifiable",
+]
